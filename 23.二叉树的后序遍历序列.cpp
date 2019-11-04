5b77bf336acfad42a0b36e32d402d35a959dfa79
deepduke <runningwater.jc@gmail.com>
2019-11-04 21:30:03 +0800

    fixed::bugs

--- 23.二叉树的后序遍历序列.cpp
+++ 23.二叉树的后序遍历序列.cpp
@@ -37,16 +37,17 @@ public:
 根据BST的定义，递归判断，后序遍历序列 = [左子树结点，右子树结点，根结点]
 递归判断左子树的结点是不是都比根结点小，右子树的结点是不是都比根结点大。
 */
-class Solution1 {
+class Solution2 {
 public:
-    bool judge(vector<int> sequence, int L, int R){
+   bool judge(vector<int> sequence, int L, int R){
         if(L >= R) return true;
         int i = L;
         for(; i < R; i++){
             if(sequence[i] > sequence[R]) break;
+    
         }
-        for(; i < R; i++){
-            if(sequence[i] <= sequence[R])
+        for(int j = i; j < R; j++){
+            if(sequence[j] <= sequence[R])
                 return false;
         }
         return judge(sequence, L, i-1) && judge(sequence, i, R-1);
